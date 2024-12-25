5d34f30de97589f939f2b2bf0f3769c2f1232ce9
Sergei Chura <s.chura@pin-up.tech>
2024-12-25 13:02:40 +0100

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'kek',
+  webDir: 'dist/kek/browser',
+};
+
+export default config;
