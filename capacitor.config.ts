fd07915fe0b37ed315cbe59f4eed6192e91fdbe4
Siarhei Chura <chura.siarhei@gmail.com>
2024-12-26 14:35:32 +0100

    initial commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'kek',
-  webDir: 'dist/kek/browser',
+  appName: 'mobile-app',
+  webDir: 'dist/mobile-app/browser',
 };
 
 export default config;
